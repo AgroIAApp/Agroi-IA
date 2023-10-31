dca73f9aabb2dfb835a4a50cb565de770e36909f
Camila Guillan <totomolino@hotmail.com>
2023-10-31 14:35:47 -0300

    fix: ndmi ranges

--- src/constants/ndviColors.js
+++ src/constants/ndviColors.js
@@ -73,37 +73,37 @@ export const NDSI_COLOR_RANGES = [
 
 export const NDMI_COLOR_RANGES = [
   {
-    min: -1, max: 0, color: '#084081', interval: '≤ 0 (Muy humedo)',
+    min: -1, max: 0, color: '#ddac61', interval: '≤ 0 (Humedad baja)',
   },
   {
-    min: 0, max: 0.1, color: '#2166ac', interval: '0 - 0.1',
+    min: 0, max: 0.1, color: '#eabf8f', interval: '0 - 0.1',
   },
   {
-    min: 0.1, max: 0.2, color: '#4393c3', interval: '0.1 - 0.2',
+    min: 0.1, max: 0.2, color: '#f8e2b2', interval: '0.1 - 0.2',
   },
   {
-    min: 0.2, max: 0.3, color: '#6baed6', interval: '0.2 - 0.3',
+    min: 0.2, max: 0.3, color: '#ffeed1', interval: '0.2 - 0.3',
   },
   {
-    min: 0.3, max: 0.4, color: '#9ecae1', interval: '0.3 - 0.4',
+    min: 0.3, max: 0.4, color: '#deebf7', interval: '0.3 - 0.4',
   },
   {
     min: 0.4, max: 0.5, color: '#c6dbef', interval: '0.4 - 0.5',
   },
   {
-    min: 0.5, max: 0.6, color: '#deebf7', interval: '0.5 - 0.6',
+    min: 0.5, max: 0.6, color: '#9ecae1', interval: '0.5 - 0.6',
   },
   {
-    min: 0.6, max: 0.7, color: '#ffeed1', interval: '0.6 - 0.7',
+    min: 0.6, max: 0.7, color: '#6baed6', interval: '0.6 - 0.7',
   },
   {
-    min: 0.7, max: 0.8, color: '#f8e2b2', interval: '0.7 - 0.8',
+    min: 0.7, max: 0.8, color: '#4393c3', interval: '0.7 - 0.8',
   },
   {
-    min: 0.8, max: 0.9, color: '#eabf8f', interval: '0.8 - 0.9',
+    min: 0.8, max: 0.9, color: '#2166ac', interval: '0.8 - 0.9',
   },
   {
-    min: 0.9, max: 1, color: '#ddac61', interval: '1 (Humedad óptima)',
+    min: 0.9, max: 1, color: '#084081', interval: '1 (Humedad óptima)',
   },
 ];
 
